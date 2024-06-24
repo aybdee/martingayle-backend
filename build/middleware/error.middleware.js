80c854a8d22985220c202941f75b0d97fa15628e
abundance <abundanceanyanwu@gmail.com>
2024-06-24 06:43:35 +0100

    added build to gitignore

--- build/middleware/error.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = void 0;
-const errorHandler = (err, req, res, next) => {
-    res
-        .status(err.statusCode ? err.statusCode : 500)
-        .json({ error: err.message });
-};
-exports.errorHandler = errorHandler;
--- build/types/bot.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ClientState = void 0;
-var ClientState;
-(function (ClientState) {
-    ClientState[ClientState["INACTIVE"] = 0] = "INACTIVE";
-    ClientState[ClientState["ACTIVE"] = 1] = "ACTIVE";
-})(ClientState || (exports.ClientState = ClientState = {}));
